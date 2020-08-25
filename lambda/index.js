967c82c40b4d23cfe48ccc5cc2adf74b812870e3
Štefan Bačkor <stevo@backor.sk>
2020-08-25 22:12:48 +0200

    Add support for webp and quality

--- lambda/index.js
+++ lambda/index.js
@@ -9,19 +9,22 @@ const Sharp = require("sharp");
 const BUCKET = process.env.BUCKET;
 const URL = process.env.URL;
 
-function resizeImage(data, width, height, format) {
+function resizeImage(data, options) {
   return Sharp(data.Body)
     .rotate()
-    .resize(width, height, { fit: "inside" })
-    .toFormat(format)
+    .resize(options.width, options.height, { fit: "inside" })
+    .toFormat(options.format, { quality: options.quality })
     .toBuffer();
 }
 
-function resizeThumbnail(data, width, height, format) {
+function resizeThumbnail(data, options) {
   return Sharp(data.Body)
     .rotate()
-    .resize(width, height, { fit: "cover", position: "attention" })
-    .toFormat(format)
+    .resize(options.width, options.height, {
+      fit: "cover",
+      position: "attention",
+    })
+    .toFormat(options.format, { quality: options.quality })
     .toBuffer();
 }
 
@@ -33,18 +36,23 @@ function format(extension) {
       return "jpeg";
     case "jpg":
       return "jpeg";
+    case "webp":
+      return "webp";
   }
 }
 
 function parseQuery(key) {
-  const match = key.match(/(.*)\/(\d+)(x|_)(\d+)\/(.*)\.(png|jpeg|jpg)/i);
+  const match = key.match(
+    /(.*)\/(\d+)(x|_)(\d+)\/(.*)\.(png|jpeg|jpg)(\.([0-9]{2}))?(\.(png|jpeg|jpg|webp))?/i
+  );
   if (match) {
     return {
       key: key,
       width: parseInt(match[2], 10),
       height: parseInt(match[4], 10),
-      format: format(match[6]),
+      format: match[10] ? format(match[10]) : format(match[6]),
       crop: match[3] === "_",
+      quality: match[8] ? parseInt(match[8], 10) : undefined,
       originalKey: `${match[1]}/original/${match[5]}.${match[6]}`,
     };
   } else {
@@ -56,13 +64,11 @@ exports.handler = function (event, context, callback) {
   const q = parseQuery(event.queryStringParameters.key);
 
   if (q) {
-    console.log(`processing ${q.originalKey}`);
+    // console.log(`processing ${q.originalKey}`, JSON.stringify(q));
     S3.getObject({ Bucket: BUCKET, Key: q.originalKey })
       .promise()
       .then((data) =>
-        q.crop
-          ? resizeThumbnail(data, q.width, q.height, q.format)
-          : resizeImage(data, q.width, q.height, q.format)
+        q.crop ? resizeThumbnail(data, q) : resizeImage(data, q)
       )
       .then((buffer) =>
         S3.putObject({
